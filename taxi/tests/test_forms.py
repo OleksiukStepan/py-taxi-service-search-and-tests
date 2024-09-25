d977d60ef6060c3a3c2d5c83d4aaa79f3d4b7141
Stepan <oleksiukstepann@gmail.com>
2024-09-25 16:35:09 +0300

    update test_forms.py

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
+from django.urls import reverse
 from taxi.forms import (
     CarForm,
     CarSearchForm,
@@ -9,7 +9,7 @@ from taxi.forms import (
     DriverLicenseUpdateForm,
     ManufacturerSearchForm,
 )
-from taxi.models import Manufacturer
+from taxi.models import Manufacturer, Car
 
 
 class FormTests(TestCase):
@@ -23,6 +23,15 @@ class FormTests(TestCase):
         self.driver2 = get_user_model().objects.create_user(
             username="driver2", password="test321", license_number="DRV12345"
         )
+
+        self.car1 = Car.objects.create(
+            model="Skyline", manufacturer=self.manufacturer
+        )
+        self.car2 = Car.objects.create(
+            model="Civic", manufacturer=self.manufacturer
+        )
+        self.car1.drivers.set([self.driver])
+        self.car2.drivers.set([self.driver2])
 
     def test_car_form_valid_data(self):
         form_data = {
@@ -52,18 +61,51 @@ class FormTests(TestCase):
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form_data)
 
-    def test_car_search_form(self):
+    def test_car_search_form_validity(self):
         form = CarSearchForm(data={"model": "Skyline"})
+        self.assertTrue(form.is_valid())
+
+    def test_car_search_form_functionality(self):
+        response = self.client.get(reverse("car-list"), {"model": "Skyline"})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.car1.model)
+        self.assertNotContains(response, self.car2.model)
+
+        response = self.client.get(reverse("car-list"), {"model": "Civic"})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.car2.model)
+        self.assertNotContains(response, self.car1.model)
+
+    def test_empty_search_form(self):
+        form = CarSearchForm(data={"model": ""})
         self.assertTrue(form.is_valid())
 
     def test_manufacturer_search_form(self):
         form = ManufacturerSearchForm(data={"name": "Nissan"})
         self.assertTrue(form.is_valid())
 
+    def test_manufacturer_search_form_functionality(self):
+        response = self.client.get(
+            reverse("manufacturer-list"), {"name": "Nissan"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.manufacturer.name)
+
     def test_driver_search_form(self):
         form = DriverSearchForm(data={"username": "driver1"})
         self.assertTrue(form.is_valid())
 
-    def test_empty_search_form(self):
-        form = CarSearchForm(data={"model": ""})
-        self.assertTrue(form.is_valid())
+    def test_driver_search_form_functionality(self):
+        response = self.client.get(
+            reverse("driver-list"), {"username": "driver1"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.driver.username)
+        self.assertNotContains(response, self.driver2.username)
+
+    def test_no_results_found_in_search(self):
+        response = self.client.get(
+            reverse("car-list"), {"model": "NonexistentModel"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No results found")
